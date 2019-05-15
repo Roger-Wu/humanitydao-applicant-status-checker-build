e4b67773046b24e5ab81597a5f0a6ca893f12637
Roger-Wu <gsetcrw@gmail.com>
2019-05-15 22:34:33 +0800

    trivial

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e681557f1a12a69dad1f4b17e0c47de7.js"
+  "./precache-manifest.166ac4cdea2dfe6dda2524dd69932088.js"
 );
 
 workbox.clientsClaim();
